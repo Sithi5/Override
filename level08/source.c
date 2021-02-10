2f74ef9fc9322e6b88d4be6468db95fc36a6413b
Florian Blanchard <florian5611@gmail.com>
2021-02-10 13:44:35 +0100

    level08 source done et solution trouvée

--- level08/source.c
+++ level08/source.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 // LOG: %s
 // Usage: %s filename
 // ./backups/.log
@@ -25,6 +26,8 @@ int main(int ac,char **av)
 FILE *backup;
 FILE *file;
 int fd;
+char c = 255;
+int t = 
 
 if (ac <= 2)
 {
@@ -43,16 +46,18 @@ if (!file)
 	printf("ERROR: Failed to open %s\n");
 	exit(1);
 }
-strncat();
-fd = open(av[1],,);
+char buffer[99] = "./backups/";
+strncat(buffer, av[1], 99 - strlen(buffer) - 1);
+//fd = open(av[1], O_CREAT | O_EXCL | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+fd = open(buffer, 0xc1, 0x1b0);
 if (!fd)
 {
-printf("ERROR: Failed to open %s%s\n","./backups/", av[1]);
-exit(1);
+    printf("ERROR: Failed to open %s%s\n","./backups/", av[1]);
+    exit(1);
 }
-write(fd,1,);
-fgets(file);
-log_wrapper("Finished back up ",backup);
+while ((c = fgetc(file)) != EOF)
+    write(fd, c, 1);
+log_wrapper("Finished back up ", backup);
 fclose(backup);
 close(fd);
 }
