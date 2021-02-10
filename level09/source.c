ef2715372fa300f1d4b46f4b15eadb7f2ddff9b6
Sithi5 <ma.sithis@gmail.com>
2021-02-10 18:20:39 +0100

    fixup

--- level09/source.c
+++ level09/source.c
@@ -6,6 +6,7 @@ struct s_struct
 {
     char message[128]; // check the size
     char username[40]; // check the size
+    unsigned int i;
 };
 
 void secret_backdoor()
@@ -22,7 +23,7 @@ void set_username(s_struct *s)
     int i = 0;
     int y = 16;
     long int tmp;
-    char username[17];
+    char username[128];
 
     bzero(username, 10);
     puts(">: Enter your username");
@@ -41,7 +42,7 @@ void set_msg(s_struct *s)
     puts(">: Msg @Unix-Dude");
     printf(">>:");
     fgets(message, 1024, stdin);
-    strncpy(s->message, message, 128);
+    strncpy(s->message, message, s->i); // s->i == 140
     return;
 }
 
@@ -50,6 +51,7 @@ void handle_msg()
     s_struct *s;
 
     bzero(s->username, 40);
+    s->i = 140;
     set_username(s);
     set_msg(s);
     puts(">: Msg sent!");
